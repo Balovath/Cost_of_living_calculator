6445e3aae5141c32a1324c68e90e63794795ee2e
Balovath <passedthelife@gmail.com>
2024-04-18 23:10:56 +0200

    release

--- Gemfile
+++ Gemfile
@@ -1,22 +1,25 @@
 source "https://rubygems.org"
 
 # Ruby version
-ruby "3.0.4"
+ruby "3.2.2"
 
-gem 'sinatra'
-gem 'activerecord'
-gem 'sinatra-activerecord'
-gem 'sinatra-contrib'
-gem 'thin'
-gem 'rake'
-gem 'faraday'
+gem "sinatra"
+gem "activerecord"
+gem "sinatra-activerecord"
+gem "sinatra-contrib"
+gem "thin"
+gem "rake"
+gem "faraday"
+gem "json"
+gem "dotenv"
+gem "byebug"
 
-group :development do 
-	gem 'sqlite3'
-	gem 'tux' # gives you access to an interactive console similar to 'rails console'
-	gem 'dotenv'
+group :development do
+  gem "sqlite3", "~> 1.4"
+  gem "tux" # gives you access to an interactive console similar to 'rails console'
+  gem "dotenv"
 end
 
-group :production do 
-	gem 'pg'
+group :production do
+  gem "pg"
 end
--- app.rb
+++ app.rb
@@ -1,79 +1,90 @@
-require 'bundler'
+require "bundler"
 Bundler.require
 
-
 module LivingCostCalc
+  class App < Sinatra::Base
 
-    class App < Sinatra::Base
+    # global settings
+    configure do
+      set :root, File.dirname(__FILE__)
+      set :public_folder, "public"
 
-        # global settings
-        configure do
-          set :root, File.dirname(__FILE__)
-          set :public_folder, 'public'
-
-          register Sinatra::ActiveRecordExtension
-        end
-
-        # development settings
-        configure :development do
-            register Sinatra::Reloader
-        end
-
-        # database settings
-        set :database_file, 'config/database.yml'
-
-        # require all models
-        Dir.glob('./lib/*.rb') do |model|
-          require model
-        end
-
-        # root route
-        get '/'  do 
-            erb :index
-        end
-
-        # start here (where the user enters their info)
-        get '/start' do 
-            erb :start
-        end
-
-        # results
-        get '/results' do
-            city = params[:city]
-            country = params[:country]
-
-            # if country or city names have spaces, process accordingly
-            esc_city = ERB::Util.url_encode(country) # e.g. "St Louis" becomes 'St%20Louis'
-            esc_country = ERB::Util.url_encode(country) # e.g. "United States" becomes 'United%20States'
-
-            url = URI("https://cost-of-living-prices-by-city-country.p.rapidapi.com/get-city?city=#{esc_city}&country=#{esc_country}")
-
-            conn = Faraday.new(
-                url: url,
-                headers: {
-                    'X-RapidAPI-Key' => ENV['RapidAPIKey'],
-                    'X-RapidAPI-Host' => ENV['RapidAPIHost']
-                }
-              )
-
-            response = conn.get
-
-            @code = response.status
-            @results = response.body
-            
-            erb :results 
-        end
-
-        # partials
-
-        helpers do
-            # define the navbar partial
-            def partial(navbar)
-              erb(navbar, layout: false)
-            end
-        end
-
+      register Sinatra::ActiveRecordExtension
     end
 
-end
+    # development settings
+    configure :development do
+      register Sinatra::Reloader
+    end
 
+    # database settings
+    set :database_file, "config/database.yml"
+
+    # require all models
+    Dir.glob("./lib/*.rb") do |model|
+      require model
+    end
+
+    # root route
+    get "/" do
+      erb :start
+    end
+
+    # start here (where the user enters their info)
+    get "/start" do
+      erb :start
+    end
+
+    def save_request(doc)
+      File.open("response.json", "w") do |file|
+        file.write(JSON.dump(doc))
+      end
+    end
+
+    def read_request
+      file = File.read("response.json")
+      response = JSON.parse(file)
+    end
+
+    # results
+    get "/results" do
+      city = params[:city]
+      country = params[:country]
+
+      # if country or city names have spaces, process accordingly
+      esc_city = ERB::Util.url_encode(city) # e.g. "St Louis" becomes 'St%20Louis'
+      esc_country = ERB::Util.url_encode(country) # e.g. "United States" becomes 'United%20States'
+
+      # Определение URL и параметров запроса
+      url = "https://cost-of-living-and-prices.p.rapidapi.com/prices"
+      params = { city_name: esc_city, country_name: esc_country }
+
+      # Создание соединения с помощью Faraday
+      connection = Faraday.new(url: url) do |conn|
+        conn.response :logger                  # Логирование запросов для отладки
+        conn.adapter Faraday.default_adapter   # Использование стандартного адаптера (Net::HTTP)
+      end
+
+      # Выполнение GET-запроса с параметрами и заголовками
+      response = connection.get do |req|
+        req.params = params
+        req.headers["X-RapidAPI-Key"] = "e1b5e72bf2mshc3929941f4f4a15p1f54bdjsn2312a4cbb70c"
+        req.headers["X-RapidAPI-Host"] = "cost-of-living-and-prices.p.rapidapi.com"
+      end
+
+      @code = response.status
+      @results = JSON.parse (response.body)
+
+      erb :results
+    end
+
+    # partials
+
+    helpers do
+      # define the navbar partial
+      def partial(navbar)
+        erb(navbar, layout: false)
+      end
+    end
+  end
+end
